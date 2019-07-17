1c5150f41b38d1ea752c01bada77920228cd7c02
Monish Deb <deb.monish@gmail.com>
2019-07-17 18:33:33 +0530

    report fix

--- CRM/CloseAccountingPeriod/Form/Report/TrialBalance.php
+++ CRM/CloseAccountingPeriod/Form/Report/TrialBalance.php
@@ -102,21 +102,13 @@ class CRM_CloseAccountingPeriod_Form_Report_TrialBalance extends CRM_Report_Form
           ),
         ),
         'filters' => array(
-          'trxn_date_month' => array(
+          'trxn_date' => array(
             'title' => ts('Financial Period End Month'),
-            'operatorType' => CRM_Report_Form::OP_SELECT,
+            'operatorType' => CRM_Report_Form::OP_DATE,
             'options' => $months,
-            'type' => CRM_Utils_Type::T_INT,
+            'type' => CRM_Utils_Type::T_DATE,
             'pseudofield' => TRUE,
             'default' => date('n', strtotime($priorDate . "+ 1 month")),
-          ),
-          'trxn_date_year' => array(
-            'title' => ts('Financial Period End Year'),
-            'operatorType' => CRM_Report_Form::OP_SELECT,
-            'options' => $years,
-            'type' => CRM_Utils_Type::T_INT,
-            'pseudofield' => TRUE,
-            'default' => date('Y', strtotime($priorDate)),
           ),
           'prior_financial_month' => array(
             'title' => ts('Prior Financial Period End Month'),
@@ -150,13 +142,82 @@ class CRM_CloseAccountingPeriod_Form_Report_TrialBalance extends CRM_Report_Form
   public function from() {
     $endDate = NULL;
     $contactID = $this->_params['contact_id_value'];
-    if (!empty($this->_params['trxn_date_month_value']) && !empty($this->_params['trxn_date_year_value'])) {
-      $endDate = date('Y-m-t', mktime(0, 0, 0, $this->_params['trxn_date_month_value'], 1, $this->_params['trxn_date_year_value']));
+    $fieldName = 'trxn_date';
+    $relative = CRM_Utils_Array::value("{$fieldName}_relative", $this->_params);
+    $from = CRM_Utils_Array::value("{$fieldName}_from", $this->_params);
+    $to = CRM_Utils_Array::value("{$fieldName}_to", $this->_params);
+    list($from, $to) = $this->getFromTo($relative, $from, $to, NULL, NULL);
+    $clauses = [];
+    if ($from) {
+      $clauses[] = "( fieldName >= $from )";
     }
-    if (!empty($this->_params['prior_financial_month_value']) && !empty($this->_params['prior_financial_year_value'])) {
-      $priorEndDate = date('Y-m-t', mktime(0, 0, 0, $this->_params['prior_financial_month_value'], 1, $this->_params['prior_financial_year_value']));
+    if ($to) {
+      $clauses[] = "( fieldName <= {$to} )";
     }
-    $this->_from = CRM_CloseAccountingPeriod_BAO_CloseAccountingPeriod::getTrialBalanceQuery($this->_aliases, TRUE, $contactID, $endDate, $priorEndDate);
+    if (!empty($clauses)) {
+      $clauses =  implode(' AND ', $clauses);
+    }
+    $params['labelColumn'] = 'name';
+    $financialAccountType = CRM_Core_PseudoConstant::get('CRM_Financial_DAO_FinancialAccount', 'financial_account_type_id', $params);
+    $financialAccountTypes = array(
+      array_search('Liability', $financialAccountType),
+      array_search('Asset', $financialAccountType)
+    );
+    $priorDate = NULL;
+    if ($contactID) {
+      $priorDate = CRM_CloseAccountingPeriod_BAO_CloseAccountingPeriod::getPriorFinancialPeriod($contactID);
+    }
+    if (empty($priorDate)) {
+      $financialBalanceField = 'opening_balance';
+    }
+    else {
+      $financialBalanceField = 'current_period_opening_balance';
+    }
+    $sql = "
+    FROM (
+      SELECT cft1.id, 0 as fid, 0 AS credit, cft1.total_amount AS debit,
+        cft1.to_financial_account_id AS financial_account_id
+        FROM civicrm_financial_trxn cft1
+        WHERE $clauses
+    ";
+    $sql = str_replace('fieldName', 'cft1.trxn_date', $sql);
+    $sql .= "
+    UNION
+    SELECT cft2.id, 0 as fid, cft2.total_amount AS credit, 0 AS debit, cft2.from_financial_account_id
+      FROM civicrm_financial_trxn cft2
+      WHERE $clauses
+    ";
+    $sql = str_replace('fieldName', 'cft2.trxn_date', $sql);
+    $sql .= "
+    UNION
+    SELECT cft3.id, cfi3.id, 0 AS credit, cfi3.amount AS debit, cfi3.financial_account_id
+      FROM civicrm_financial_item cfi3
+        INNER JOIN civicrm_entity_financial_trxn ceft3 ON cfi3.id = ceft3.entity_id
+          AND ceft3.entity_table = 'civicrm_financial_item'
+        INNER JOIN civicrm_financial_trxn cft3 ON ceft3.financial_trxn_id = cft3.id
+          AND cft3.to_financial_account_id IS NULL
+      WHERE $clauses
+    ";
+    $sql = str_replace('fieldName', 'cft3.trxn_date', $sql);
+    $sql .= "
+    UNION
+    SELECT cft4.id, cfi4.id,  cfi4.amount AS credit, 0 AS debit, cfi4.financial_account_id
+      FROM civicrm_financial_item cfi4
+      INNER JOIN civicrm_entity_financial_trxn ceft4 ON cfi4.id=ceft4.entity_id
+        AND ceft4.entity_table='civicrm_financial_item'
+      INNER JOIN civicrm_financial_trxn cft4 ON ceft4.financial_trxn_id=cft4.id
+        AND cft4.from_financial_account_id IS NULL
+      WHERE $clauses
+    UNION
+    SELECT 0 as tid, 0 as fid, IF (financial_account_type_id = " . array_search('Liability', $financialAccountType) . ", {$financialBalanceField}, 0) AS credit, IF (financial_account_type_id = " . array_search('Asset', $financialAccountType) . ", {$financialBalanceField}, 0) AS debit, cfa5.id
+      FROM civicrm_financial_account cfa5
+      INNER JOIN civicrm_financial_accounts_balance cfab ON cfab.financial_account_id = cfa5.id
+      WHERE cfa5.financial_account_type_id IN (" . implode(', ', $financialAccountTypes) . ") AND {$financialBalanceField} <> 0
+  ) AS {$this->_aliases['civicrm_financial_trxn']}
+  INNER JOIN civicrm_financial_account {$this->_aliases['civicrm_financial_account']} ON {$this->_aliases['civicrm_financial_trxn']}.financial_account_id = {$this->_aliases['civicrm_financial_account']}.id
+    ";
+    $sql = str_replace('fieldName', 'cft4.trxn_date', $sql);
+    $this->_from = $sql;
   }
 
   public function orderBy() {
